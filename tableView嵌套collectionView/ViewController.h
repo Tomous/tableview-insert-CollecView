3289b19391ff409c13e6d9c5ff54c6df691a7060
wenhua <xudacheng_tomous@163.com>
2018-03-01 19:39:39 +0800

    Initial Commit

---
+++ tableView嵌套collectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableView嵌套collectionView
+//
+//  Created by wenhua on 2018/3/1.
+//  Copyright © 2018年 wenhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tableView嵌套collectionViewTests/tableView__collectionViewTests.m
@@ -0,0 +1,39 @@
+//
+//  tableView__collectionViewTests.m
+//  tableView嵌套collectionViewTests
+//
+//  Created by wenhua on 2018/3/1.
+//  Copyright © 2018年 wenhua. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tableView__collectionViewTests : XCTestCase
+
+@end
+
+@implementation tableView__collectionViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
